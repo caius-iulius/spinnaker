9a35f54e08a2251ccb7dd2d8fa065bccfac01daf
Caius Iulius Caesar <caiusiuliuscaesar84@gmail.com>
2022-08-04 15:58:31 +0200

    l'interprete usa solo liste per i binding

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,37 +1,36 @@
 module Interpreter where
-import System.IO
-import System.Exit
-import qualified Data.Map as Map
+import System.IO(hFlush, stdout)
+import System.Exit(exitSuccess)
 import Control.Monad.Reader
-import Data.Char
-import Data.Tree
+import Data.Char(ord, chr)
+import Data.Tree(drawTree)
 
 import MPCL(StdCoord(..), dummyStdCoord)
-import PrettyPrinter
+import PrettyPrinter(toTreeHLExpr)
 import TypingDefs
 import HLDefs
 
 
-type InterpEnv = Map.Map String HLExpr
-type InterpState t = ReaderT InterpEnv IO t
+type InterpBinds = [(String, HLExpr)]
+type InterpState t = ReaderT InterpBinds IO t
 
 
-sievePatternInner :: HLPatternData -> HLExpr -> Maybe (Map.Map String HLExpr)
-sievePatternInner PatWildcard _ = return Map.empty
+sievePatternInner :: HLPatternData -> HLExpr -> Maybe InterpBinds
+sievePatternInner PatWildcard _ = return []
 sievePatternInner (PatLiteral plit) (_, _, ExprLiteral elit) =
-    if plit == elit then return Map.empty else Nothing
+    if plit == elit then return [] else Nothing
 sievePatternInner (PatVariant pvar ps) (_, _, ExprConstructor evar es) =
     if pvar == evar then do
         maps <- zipWithM sievePattern ps es
-        return $ Map.unions maps
+        return $ concat maps
     else Nothing
 sievePatternInner p e = error $ show p ++ " : \n\n" ++ (drawTree $ toTreeHLExpr e)
 
-sievePattern :: HLPattern -> HLExpr -> Maybe (Map.Map String HLExpr)
+sievePattern :: HLPattern -> HLExpr -> Maybe InterpBinds
 sievePattern (_, Nothing, patdata) e = sievePatternInner patdata e
 sievePattern (_, Just l, patdata) e = do
     inner <- sievePatternInner patdata e
-    return $ Map.union inner (Map.singleton l e)
+    return $ (l,e):inner
 
 getBindsInner :: HLPatternData -> [String]
 getBindsInner PatWildcard = []
@@ -42,22 +41,27 @@ getBinds :: HLPattern -> [String]
 getBinds (_, Nothing, patdata) = getBindsInner patdata
 getBinds (_, Just l, patdata) = l : getBindsInner patdata
 
-exprSubstApply :: Map.Map String HLExpr -> HLExpr -> HLExpr
+mapDelete k [] = []
+mapDelete k ((mk,mv):kvs)
+    | k == mk = mapDelete k kvs
+    | otherwise = (mk,mv) : mapDelete k kvs
+
+exprSubstApply :: InterpBinds -> HLExpr -> HLExpr
 exprSubstApply _ e@(_, _, ExprLiteral _) = e
 exprSubstApply s (c, dt, ExprApp e0 e1) = (c, dt, ExprApp (exprSubstApply s e0) (exprSubstApply s e1))
 exprSubstApply s e@(_, _, ExprLabel l) =
-    case Map.lookup l s of
+    case lookup l s of
         Nothing -> e
         Just expr -> expr
 exprSubstApply s (c, dt, ExprConstructor v es) = (c, dt, ExprConstructor v (map (exprSubstApply s) es))
 exprSubstApply s (c, dt, ExprCombinator v es) = (c, dt, ExprCombinator v (map (exprSubstApply s) es))
 exprSubstApply s (c, dt, ExprLambda pat ret) =
     let
-        s' = foldl (flip Map.delete) s (getBinds pat)
+        s' = foldl (flip mapDelete) s (getBinds pat)
     in (c, dt, ExprLambda pat (exprSubstApply s' ret))
 exprSubstApply s (c, dt, ExprPut val pses) =
     let
-        peSubstApply (p, e) = (p, exprSubstApply (foldl (flip Map.delete) s (getBinds p)) e)
+        peSubstApply (p, e) = (p, exprSubstApply (foldl (flip mapDelete) s (getBinds p)) e)
     in (c, dt, ExprPut (exprSubstApply s val) (map peSubstApply pses))
 
 builtinApply :: String -> [HLExpr] -> InterpState HLExprData
@@ -102,9 +106,7 @@ eval (_, _, ExprApp f a) = do
         (c, myt, myf) -> error $ show c ++ " WHAT: " ++ show myf ++ " : " ++ show myt
 eval e@(c, dt, ExprLabel l) = do
     env <- ask
-    case Map.lookup l env of
-        Just expr -> eval expr
-        Nothing -> return e
+    let Just expr = lookup l env in eval expr
 eval e@(c, dt, ExprConstructor l es) = do
     es' <- mapM eval es
     return (c, dt, ExprConstructor l es')
@@ -119,7 +121,6 @@ eval e@(c, dt, ExprPut val pses) = do
 
 evalProgram :: (HLExpr, [(String, HLExpr)]) -> IO HLExpr
 evalProgram (entryPoint, defs) = do
-    let env = Map.fromList defs
     putStrLn $ "\n---- ESEGUO IL PROGRAMMA ----"
-    runReaderT (eval entryPoint) env
+    runReaderT (eval entryPoint) defs
     --TODO: così main può avere soltanto il tipo: (), il che non viene controllato nel typer
